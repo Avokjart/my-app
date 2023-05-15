6ec9032977b68c8211bbe1e284cf2fdb9f72ac87
avokjart <slobodance15@gmail.com>
2023-05-15 14:34:13 +0200

    rating fixat

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-export default function Movie (props) {
+export default function Movie(props) {
+    const renderStars = () => {
+        const stars = [];
+        
+        for (let i = 0; i < props.item.rating; i++) {
+            stars.push(<img key={i} src="star.png" alt="Star" className="star" style={{ marginRight: '5px', width: '20px', height: '20px'}} />);
+        }
+        return stars;
+    };
+
     return (
         <li className="list-group-item">
-            { props.item.title }
-            <button className="btn btn-sm btn-danger float-end" onClick={() => {props.deleteItem(props.item.id)} }>X</button>
+            {props.item.title}
+            <div className="rating">
+                {renderStars()}
+            </div>
+            <img
+                src="delete.png"
+                alt="Delete"
+                className="btn-delete"
+                style={{ marginRight: '5px', width: '20px', height: '20px' }}
+                onClick={() => props.deleteItem(props.item.id)}
+            />
         </li>
-    )
+    );
 }
--- src/MovieList.js
+++ src/MovieList.js
@@ -2,37 +2,61 @@ import React, { useState, useRef } from 'react';
 import Movie from './Movie';
 
 export default function MovieList() {
-    const [movies, setMovies] = useState([{
-        id: 1,
-        title: "First item"
-    }]);
-    const inputRef = useRef();
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "First item",
+      rating: "3"
+    }
+  ]);
+  const inputRef = useRef();
+  const ratingRef = useRef();
 
-    function addItem (event){
-        if (event.keyCode === 13) {
-            const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
+  function addItem() {
+    const title = inputRef.current.value;
+    const rating = parseInt(ratingRef.current.value);
 
-            setMovies([...movies, {
-                id: newId,
-                title: inputRef.current.value 
-            }])
-
-            inputRef.current.value = "";
-        }
-
+    if (title === "" || rating === 0) {
+      alert("Ange både titel och betyg för att spara filmen.");
+      return;
     }
 
-    function deleteItem(id) {
-        setMovies(movies.filter((item) => item.id !== id));
+    const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
 
-    }
+    setMovies(prevMovies => [
+      ...prevMovies,
+      {
+        id: newId,
+        title: title,
+        rating: rating
+      }
+    ]);
 
-    return (
-        <div>
-            <input className="form-control" placeholder="Add new a movie here..." ref={inputRef} onKeyUp={addItem} />
-            <ul class="list-group">
-                { movies.map(movie => <Movie key={movie.id} item={movie} deleteItem={deleteItem}  />)}
-            </ul>
-        </div>
-    )
+    inputRef.current.value = "";
+    ratingRef.current.value = 0;
+  }
+
+  function deleteItem(id) {
+    setMovies(prevMovies => prevMovies.filter(item => item.id !== id));
+  }
+
+  return (
+    <div>
+      <input className="form-control" placeholder="Add a new movie here..." ref={inputRef} />
+      <select ref={ratingRef}>
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button onClick={addItem}>Add Movie</button>
+      <ul className="list-group">
+        {movies.map(movie => (
+          <Movie key={movie.id} item={movie} deleteItem={deleteItem} />
+        ))}
+      </ul>
+    </div>
+  );
 }
